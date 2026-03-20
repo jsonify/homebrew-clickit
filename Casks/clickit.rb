7fee6eb42483c1b2cc65f9b53806e86256297153
jsonify <phlacin@gmail.com>
2026-03-20 12:33:22 -0700

    Refactor formatting and cleanup in ClickIt Cask

--- Casks/clickit.rb
+++ Casks/clickit.rb
@@ -1,23 +1,18 @@
 cask "clickit" do
-  version "1.6.3"
-  sha256 "PLACEHOLDER_WILL_BE_SET_BY_CI"
+    version "1.6.3"
+    sha256 "PLACEHOLDER_WILL_BE_SET_BY_CI"
 
-  url "https://github.com/jsonify/ClickIt/releases/download/v#{version}/ClickIt-#{version}.dmg"
-  name "ClickIt"
-  desc "Native macOS auto-clicker with precision timing and window targeting"
-  homepage "https://github.com/jsonify/ClickIt"
+url "https://github.com/jsonify/ClickIt/releases/download/v#{version}/ClickIt-#{version}.dmg" 
+name "ClickIt" 
+desc "Native macoS auto-clicker with precision timing and window targeting" 
+homepage "https://github.com/jsonify/ClickIt"
 
-  app "ClickIt.app"
+app "ClickIt.app"
 
-  postinstall do
-    system_command "/usr/bin/xattr",
-                   args: ["-cr", "#{appdir}/ClickIt.app"]
-  end
+uninstall quit: "com.jsonify.clickit"
 
-  uninstall quit: "com.jsonify.clickit"
-
-  zap trash: [
+zap trash: [
     "~/Library/Application Support/ClickIt",
     "~/Library/Preferences/com.jsonify.clickit.plist",
-  ]
+]
 end
